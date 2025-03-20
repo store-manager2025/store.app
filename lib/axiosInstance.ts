0ebc6892bc560d8d10260aa38006a4a25f4984fa
Jehoon <limjhoon8@gmail.com>
2025-03-20 23:53:54 +0900

    ✨ Feat : 실제 서버 주소 변경

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8383"; // 백엔드 서버 주소
+const API_BASE_URL = "http://52.79.57.150:8383"; // 백엔드 서버 주소
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
